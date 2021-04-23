39b97950d732ff6165f9fe214ab0e711dd30cf2c
aniketjha1729 <kumaraniket1729@gmail.com>
2021-04-23 21:21:06 +0530

    redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-import axios from "../../api/axios";
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -18,22 +20,9 @@ const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("password does not match");
+      setAlert("password does not match", "danger");
     } else {
-      const newUser = { name, email, password };
-      try {
-        const config = {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        };
-        const body = JSON.stringify(newUser);
-
-        const res = await axios.post("/api/users/register", body, config);
-        console.log(res.data);
-      } catch (err) {
-        console.log(err.response.data);
-      }
+      console.log("Error");
     }
   };
 
@@ -96,4 +85,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
