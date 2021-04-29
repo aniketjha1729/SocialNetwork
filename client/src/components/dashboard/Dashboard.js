556d8aa2fbf3d0c2667b1c9ad4470d663915aa1b
aniketjha1729 <kumaraniket1729@gmail.com>
2021-04-30 01:44:20 +0530

    edit

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Spinner from "../layout/Spinner";
 import { Link } from "react-router-dom";
+import { DashboardAction } from "./DashboardAction";
 
 const Dashboard = ({
   getCurrentProfile,
@@ -24,74 +25,7 @@ const Dashboard = ({
       </p>
       {profile !== null ? (
         <Fragment>
-          <div className="dash-buttons">
-            <Link to="/edit-profile" className="btn btn-light">
-              <i className="fas fa-user-circle text-primary"></i> Edit Profile
-            </Link>
-            <Link to="/add-experience" className="btn btn-light">
-              <i className="fab fa-black-tie text-primary"></i> Add Experience
-            </Link>
-            <Link href="/add-education" className="btn btn-light">
-              <i className="fas fa-graduation-cap text-primary"></i> Add
-              Education
-            </Link>
-          </div>
-          <h2 className="my-2">Experience Credentials</h2>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Company</th>
-                <th className="hide-sm">Title</th>
-                <th className="hide-sm">Years</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Tech Guy Web Solutions</td>
-                <td className="hide-sm">Senior Developer</td>
-                <td className="hide-sm">02-03-2009 - 01-02-2014</td>
-                <td>
-                  <button className="btn btn-danger">Delete</button>
-                </td>
-              </tr>
-              <tr>
-                <td>Traversy Media</td>
-                <td className="hide-sm">Instructor & Developer</td>
-                <td className="hide-sm">02-03-2015 - Now</td>
-                <td>
-                  <button className="btn btn-danger">Delete</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <h2 className="my-2">Education Credentials</h2>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>School</th>
-                <th className="hide-sm">Degree</th>
-                <th className="hide-sm">Years</th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Northern Essex</td>
-                <td className="hide-sm">Associates</td>
-                <td className="hide-sm">02-03-2007 - 01-02-2009</td>
-                <td>
-                  <button className="btn btn-danger">Delete</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <div className="my-2">
-            <button className="btn btn-danger">
-              <i className="fas fa-user-minus"></i>
-              Delete My Account
-            </button>
-          </div>
+          <DashboardAction />
         </Fragment>
       ) : (
         <Fragment>
@@ -114,6 +48,7 @@ Dashboard.propTypes = {
 const mapStateToProps = (state) => ({
   auth: state.auth,
   profile: state.profile,
+  
 });
 
 export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
